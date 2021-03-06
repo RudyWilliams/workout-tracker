2285cea5c641e50451e1fea13a0c77f14c4d5588
Rudy Williams <rudysw05@knights.ucf.edu>
2021-03-06 12:47:26 -0500

    starts templates and basic home, user pgs

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Home Page"
+    return render_template("home.html")
+
+
+@app.route("/user/<int:user_id>")
+def user(user_id):
+    return render_template("user.html", user_id=user_id)
 
 
 if __name__ == "__main__":
