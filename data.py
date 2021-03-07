8b63a3902786f5a0a2523e8b963b476a25ff69dd
Rudy Williams <rudysw05@knights.ucf.edu>
2021-03-07 12:23:32 -0500

    adds sample data to build user page

---
+++ data.py
@@ -0,0 +1,16 @@
+data = [
+    {
+        "user_id": 1,
+        "name": {
+            "user": "rw",
+            "first": "Rudy",
+            "middle": None,
+            "last": "Williams"
+        },
+        "interests": {
+            "activities": ["surfing","running","sports","programming"],
+            "foods": ["pizza","tacos","rice bowls"],
+        }
+        "connections": None,
+    }
+]
