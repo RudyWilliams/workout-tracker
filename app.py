82c89116aa5c7a3f389fb99767db3b249b0195a7
Rudy Williams <rudysw05@knights.ucf.edu>
2021-03-07 13:15:15 -0500

    adds more html pages

--- app.py
+++ app.py
@@ -5,8 +5,24 @@ app = Flask(__name__)
 
 
 @app.route("/")
+@app.route("/home")
 def home():
     return render_template("home.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 
 @app.route("/user/<int:user_id>")
